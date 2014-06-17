c7a4556df02fbb178e4fba4f775e7e780a6b09de
doo2 <iamdoo2@gmail.com>
2014-06-17 07:56:11 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Likelion2nd::Application.config.secret_token = '6a46cbda16c3503efaac4129bd350e003b70ebd31c7b325a1a9e0a623502413a67f26736da0226ada453b8051a4cad1e3ce4fbe3dc0acb71c2a6adfc229123d4'
